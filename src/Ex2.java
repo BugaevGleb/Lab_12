6bbefa4a0f70f528ee4e5e06388523cee10c647a
BugaevGleb <b.gleb02@mail.ru>
2022-11-14 15:40:59 +0300

    1

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Ex2 {
 
@@ -6,25 +7,26 @@ public class Ex2 {
 
         try
         {
-            BufferedReader input = new BufferedReader(new FileReader("E:\\Innopolis\\Programming\\ITP\\Java\\Lab_12_14.07.2022\\input2.txt"));
+
+            Scanner scan = new Scanner(new File("E:\\Innopolis\\Programming\\ITP\\Java\\Lab_12_14.07.2022\\input2.txt"));
 
             int a, b;
 
-            String s1 = input.readLine();
+            String s1 = scan.next();
             a = Integer.parseInt(s1);
 
-            String s2 = input.readLine();
+            String s2 = scan.next();
             b = Integer.parseInt(s2);
 
             int c = a / b;
             System.out.println(c);
 
-        } catch(IOException ex){
-            System.out.println("Troubles with file");
         } catch(NumberFormatException ex){
             System.out.println("Troubles with parsing");
         } catch(ArithmeticException ex){
             System.out.println("Division by zero");
+        } catch(IOException ex){
+            System.out.println("Troubles with file");
         }
 
 
